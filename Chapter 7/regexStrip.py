0f4000a923f3439746df81f53fcd3780ba95001c
John Pappas <john@johnicboom.com>
2025-04-08 17:01:06 -0500

    Ch7 3rd Proj 2nd Draft

--- Chapter 7/regexStrip.py
+++ Chapter 7/regexStrip.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# regexStrip.py - regex version of the strip() method
+# regexStrip2.py - regex version of the strip() method
 
 """
 Write a function that takes a string and does the same thing as
@@ -17,26 +17,13 @@ def regexStrip(inputText, rmChars = r'\s'):
     # Copy inputText to working string for non-destructive work
     cleanText = inputText
     
-    # If rmChars has more than one character, I need to add |
-    # and ^ or $
-    if len(rmChars) > 1 and (rmChars != r'\s' and rmChars != r'\d' and rmChars != r'\w'):
-        rmCharsS = rmChars[0]
-        for i in range(len(rmChars) - 1):
-            rmCharsS = rmCharsS + '|^' + rmChars[i+1]
-            
-        rmCharsE = rmChars[0]
-        for i in range(len(rmChars) - 1):
-            rmCharsE = rmCharsE + '$|' + rmChars[i+1]
-    else:
-        rmCharsS, rmCharsE = rmChars, rmChars
-        
+    # Put the characters in a regex class by wrapping with []
+    rmChars = '[' + rmChars + ']'
                 
     # Regex that matches any string starting with rmChars
-    #removalS = re.compile('^' + rmChars + '.*', re.DOTALL)
-    removalS = re.compile('^' + rmCharsS)
+    removalS = re.compile('^' + rmChars)
     # Regex that matches any string ending with rmChars
-    #removalE = re.compile('.*' + rmChars + '$', re.DOTALL)
-    removalE = re.compile(rmCharsE + '$')
+    removalE = re.compile(rmChars + '$')
     
     # If I get a match, remove first char, then check again
     match = removalS.findall(cleanText)
@@ -51,13 +38,9 @@ def regexStrip(inputText, rmChars = r'\s'):
         cleanText = cleanText[0:len(cleanText) - 1]
         match = removalE.findall(cleanText)
     
-    # Print final cleaned string
-    #print(f'Original text was: "{inputText}"')
-    #print(f'Stripped text is: "{cleanText}"')
-    
     return cleanText
 
-# Test
+# Tests
 test = '   This is a test  '
 print(f'Original text: "{test}"\nCleaned text:  "{regexStrip(test)}"')
 
@@ -66,11 +49,9 @@ print(f'Original text: "{test}"\nCleaned text:  "' + regexStrip(test, 'maps') + 
 
 test = ' \n This\nis\na\ntest \n '
 print(f'Original text: "{test}"\nCleaned text:  "{regexStrip(test)}"')
-# regexStrip(' \n This\nis\na\ntest \n ')
 
 test = '234Test567'
 print(f'Original text: "{test}"\nCleaned text:  "' + regexStrip(test, r'\d') + '"')
-#regexStrip('234Test567', '\d')
 
 test = 'qwerty123 Test Text 456asdfg'
 print(f'Original text: "{test}"\nCleaned text:  "' + regexStrip(test, r'\w') + '"')
